58bc319bbb5fd3810144d3df4e6665120cd2e962
HyeonJun-GitHub <61862658+HyeonJun-GitHub@users.noreply.github.com>
2020-05-06 22:39:59 +0900

    Ground & Enemy

---
+++ GroundTowerDefense/Assets/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        //normalized 동일한 속도
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+
+        if (wavePointIndex >= Waypoints.points.Length -1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavePointIndex++;
+        target = Waypoints.points[wavePointIndex];
+    }
+
+
+    
+
+}
